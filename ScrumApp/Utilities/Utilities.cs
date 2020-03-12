797e601b96cc8d6f1f0bdb3f822d0e46c31f7087
william ganrot <williamganrot@hotmail.com>
2020-03-12 16:29:13 +0100

    add board active link

---
+++ ScrumApp/Utilities/Utilities.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScrumApp.Utilities
+{
+    public static class Utilities
+    {
+        public static string IsBoardActive(this IHtmlHelper htmlHelper, string controller, string action, string board)
+        {
+
+            var routeData = htmlHelper.ViewContext.RouteData;
+
+            var routeAction = routeData.Values["action"].ToString();
+            var routeController = routeData.Values["controller"].ToString();
+            var routeBoard = routeData.Values["boardSlug"].ToString();
+
+            var returnActive = (controller == routeController && (action == routeAction) && (board == routeBoard));
+            System.Diagnostics.Debug.WriteLine(board + " " + returnActive);
+            return returnActive ? "active" : "";
+        }
+    }
+}
